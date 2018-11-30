df07f425c04208aa00c2eb04b8d8d2ebfefd775c
baikadamovagauhar <43268693+baikadamovagauhar@users.noreply.github.com>
2018-11-30 22:07:22 +0600

    Add files via upload

---
+++ week12/task1.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php
+if (isset($_GET['name'])){
+    setcookie("username", $_GET['name']);
+    echo "HeLLo,dear ".$_GET["name"]." !";
+}
+else{
+    echo "<form action=\"task1.php\">
+    Enter your name <input type=\"text\" name=\"name\">
+    <input type=\"submit\">
+</form>";
+}
+
+
+?>
+</body>
+</html>
